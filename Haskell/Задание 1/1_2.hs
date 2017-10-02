d4dc2dd49d07daaa6efb3028e82f0a897da2e873
Pavel Epanechkin <32260258+pavel-epanechkin@users.noreply.github.com>
2017-10-02 15:29:32 +0300

    Update 1_2.hs

--- Haskell/Задание 1/1_2.hs
+++ Haskell/Задание 1/1_2.hs
@@ -7,22 +7,22 @@ sinus x =
 --НОД двух целых чисел
 nod :: Integer -> Integer -> Integer
 nod a b | (a < 0 || b < 0) = nod (abs a) (abs b)
-		| a < b = nod b a
-		| a >= b = if (a `mod` b == 0) then b else nod b (a `mod` b)
+	| a < b = nod b a
+	| a >= b = if (a `mod` b == 0) then b else nod b (a `mod` b)
 
 --Возведение целого числа в целую степень
 pow :: Integer -> Integer -> Double
 pow m n = 
 	let powNat m n | n == 0 = 1
-				   | m == 0 = 0
-				   | n `mod` 2 == 1 = m * powNat m (n - 1)
-				   | n `mod` 2 == 0 = powNat (m * m) (n `div` 2) in
-    if n >= 0 then toDouble (powNat m n) else  1 / toDouble (powNat m (-1 * n))
+		       | m == 0 = 0
+		       | n `mod` 2 == 1 = m * powNat m (n - 1)
+		       | n `mod` 2 == 0 = powNat (m * m) (n `div` 2) in
+    	if n >= 0 then toDouble (powNat m n) else  1 / toDouble (powNat m (-1 * n))
 	where toDouble x = fromIntegral x::Double
 
 --Возведение целого числа в натуральную степень
 powN :: Integer -> Integer -> Integer
 powN m n | n == 0 = 1
-		 | m == 0 = 0
-		 | n `mod` 2 == 1 = m * powN m (n - 1)
-		 | n `mod` 2 == 0 = powN (m * m) (n `div` 2)
+	 | m == 0 = 0
+	 | n `mod` 2 == 1 = m * powN m (n - 1)
+	 | n `mod` 2 == 0 = powN (m * m) (n `div` 2)
